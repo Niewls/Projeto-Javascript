c3095fc08101908cb2cea7290dd9abb4052557c3
Daniel Pires <nielpires2014@gmail.com>
2023-08-11 14:59:30 -0300

    Projeto Javascript 2.0

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+function Start(){
 const nome = document.getElementById("nome");
 const sobrenome = document.getElementById("sobrenome");
 const idade = document.getElementById("idade");
 const peso = document.getElementById("peso");
 const botao = document.getElementById ("data-input");
 getInput(botao, nome, sobrenome, idade, peso);
+}
 
 function getInput(botao, nome, sobrenome, idade, peso){
+    let pessoas = [];
     botao.addEventListener('click', function(){
         const Obj = Constructor(nome, sobrenome, idade, peso);
-        console.log(Obj);
-        alert("enviado com sucesso!")
+        pessoas.push(Obj);
+        console.log(pessoas);
+        alert("enviado!");
+        Reset(nome, sobrenome, idade, peso)
     })
 }
 
@@ -21,5 +26,16 @@ function Constructor(nome, sobrenome, idade, peso){
         peso: peso.value,
     }
 }
+
+function Reset (nome, sobrenome, idade, peso){
+    nome.value = '';
+    sobrenome.value = '';
+    idade.value = '';
+    peso.value = '';
+}
+
+Start();
+
+
 
 
